9cf011c7858340461c89d964cd217cd6d00c2212
Максименко Евгений Геннадьевич <evgenij.maksimenko.01@privatbank.ua>
2015-12-21 21:39:25 +0200

    fixed extension saved file

--- src/gontsov/FileMenu.java
+++ src/gontsov/FileMenu.java
@@ -3,6 +3,7 @@ package gontsov;
 import javax.imageio.ImageIO;
 import javax.swing.*;
 import javax.swing.filechooser.FileFilter;
+import javax.swing.filechooser.FileNameExtensionFilter;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
@@ -30,17 +31,57 @@ public class FileMenu extends JMenuBar {
     }
 
     private class SaveItem implements ActionListener {
+        private static final String DELIMITER = ".";
+
         @Override
         public void actionPerformed(ActionEvent e) {
             JFileChooser fs = new JFileChooser();
             fs.setDialogTitle("Save a File");
+
+            String[] extensions = {"png", "bmp", "jpg", "jpeg"};
+            for (String extension : extensions) {
+                fs.addChoosableFileFilter(new FileTypeFilter(extension));
+            }
+            fs.setAcceptAllFileFilterUsed(false);
+
+            String extensionActual = fs.getFileFilter().getDescription();
+            File fileExpected = null;
+            String extensionExpected = null;
             if (fs.showSaveDialog(null) == JFileChooser.APPROVE_OPTION) {
                 try {
-                    ImageIO.write(conf.image, "png", fs.getSelectedFile());
+                    for (String extension : extensions) {
+                        if (extensionActual.equals(extension)) {
+                            fileExpected = new File(fs.getSelectedFile().getPath() + DELIMITER + extension);
+                            extensionExpected = extension;
+                        }
+                    }
+
+                    ImageIO.write(conf.image, extensionExpected, fileExpected);
                 } catch (IOException e1) {
                     e1.printStackTrace();
                 }
             }
+        }
+    }
+
+    private class FileTypeFilter extends FileFilter {
+        public String extension;
+
+        public FileTypeFilter(String extension) {
+            this.extension = extension;
+        }
+
+        @Override
+        public boolean accept(File f) {
+            if (f.isDirectory()) {
+                return true;
+            }
+            return f.getName().endsWith(extension);
+        }
+
+        @Override
+        public String getDescription() {
+            return extension;
         }
     }
 
@@ -56,28 +97,6 @@ public class FileMenu extends JMenuBar {
                     e1.printStackTrace();
                 }
             }
-        }
-    }
-
-    private class FileTypeFilter extends FileFilter {
-        String extension, description;
-
-        public FileTypeFilter(String extension, String description) {
-            this.extension = extension;
-            this.description = description;
-        }
-
-        @Override
-        public boolean accept(File f) {
-            if (f.isDirectory()) {
-                return true;
-            }
-            return f.getName().endsWith(extension);
-        }
-
-        @Override
-        public String getDescription() {
-            return description + String.format(" (*%s)", extension);
         }
     }
 }
